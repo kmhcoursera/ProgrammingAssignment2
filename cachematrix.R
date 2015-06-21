843a162c3fd3638cfd2db3d0d0d114eaad3278fa
Klaus Marius Hansen <klaus@mariushansen.dk>
2015-06-21 20:45:06 +0200

    Implementation of programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions to invert a matrix and 
+## cache the inverted matrix such that repeated invocations
+## will use the cached value
+##
+## Example
+##
+##   A = matrix(c(2,3,4,5), 2, 2)
+##   cA = makeCacheMatrix(A)
+##   B = cacheSolve(cA)
+##
+## B will now be the inverse of A (which can be checked by
+## multiplying A and B:
+##   A %*% B
+## )
 
-## Write a short comment describing this function
-
+## Create a wrapper for a matrix (that works with cacheSolve below).
+## The matrix is assumed to be square and invertible
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(value) inv <<- value
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Given a matrix wrapped by "makeCacheMatrix", return the
+## inverse of the matrix. If called multiple times, a cached
+## value will be returned
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
